b2f3287e9dfc46e1204a08ac67b24e54641485bc
narakum <0603skfk@gmail.com>
2025-08-14 18:44:21 +0900

    bottari 세팅중

---
+++ src/main/java/com/example/BottariApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BottariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BottariApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/vo/BottariVO.java
@@ -0,0 +1,13 @@
+package com.example.vo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BottariVO {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BottariVO.class, args);
+	}
+
+}
