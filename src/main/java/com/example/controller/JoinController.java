ae238ffac81d99e1d6ad26bbe266b48753ff98a1
ahah2025 <money202590637@gmail.com>
2025-08-16 22:36:26 +0900

    join

---
+++ src/main/java/com/example/controller/JoinController.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class JoinController {
+	
+	@RequestMapping(value="/join", method= {RequestMethod.GET, RequestMethod.POST})
+	public String Join(){
+		System.out.println("JoinController");
+		
+		
+		return "shop/join";	
+	}
+}
