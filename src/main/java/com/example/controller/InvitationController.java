93f883cfd7940ac26a0866e1eade235c7b64e8d1
nara-cum <0603skfk@gmail.com>
2025-08-17 10:22:11 +0900

    초대장

--- src/main/java/com/example/controller/InvitationController.java
+++ src/main/java/com/example/controller/InvitationController.java
@@ -7,12 +7,30 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class InvitationController {
 	
+	@RequestMapping(value="/invitationList", method= {RequestMethod.GET, RequestMethod.POST})
+	public String InvitationList(){
+		System.out.println("InvitationController.InvitationList()");
+		
+		
+		return "invitation/invitationList";
+		
+	}
+	
 	@RequestMapping(value="/invitation", method= {RequestMethod.GET, RequestMethod.POST})
 	public String Invitation(){
-		System.out.println("InvitationController");
+		System.out.println("InvitationControllerInvitation()");
 		
 		
 		return "invitation/invitation";
+		
+	}
+	
+	@RequestMapping(value="/invitationForm", method= {RequestMethod.GET, RequestMethod.POST})
+	public String InvitationForm(){
+		System.out.println("InvitationController.InvitationForm()");
+		
+		
+		return "invitation/invitationForm";
 		
 	}
 
